b96913f65f8c8de6cc31a919de2482e1b63dd550
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-21 02:33:39 -0400

    base web

---
+++ Sites/consultas/consulta_1.php
@@ -0,0 +1,44 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+  $fecha_seleccionada = $_POST["fecha_seleccionada"];
+
+ 	#$query = "SELECT * FROM pokemones where id = $id_nuevo;";
+    #$query = "SELECT clientes.nombre, calle, numero_domicilio, comuna, region, 
+    #fecha_entrega 
+    #FROM despachos, pedidos, clientes, direccion_clientes 
+    #WHERE despachos.id_compra = pedidos.id_compra
+    #AND pedidos.id_cliente = clientes.id_cliente
+    #AND direccion_clientes.id_cliente = clientes.id
+    #AND DATE(fecha_entrega) = $fecha_seleccionada;"
+
+    $query = "SELECT clientes.nombre, clientes.calle, clientes.numero_domicilio,
+    clientes.comuna
+    FROM clientes, compras, despachos
+    WHERE clientes.id_cliente = compras.id_cliente
+    AND despachos.id_compra = compras.id_compra
+    AND despachos.fecha_entrega = $fecha_seleccionada;"
+
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$clientes = $result -> fetchAll();
+  ?>
+
+	<table>
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+
+    </tr>
+  <?php
+	foreach ($clientes as $cliente) {
+  		echo "<tr><td>$cliente[0]</td><td>$cliente[1]</td></tr>";
+	}
+  ?>
+	</table>
+
+<?php include('../templates/footer.html'); ?>
