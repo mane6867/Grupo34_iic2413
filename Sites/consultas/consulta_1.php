b9d6dbb2d045b75ec96fe3a9b03a8f287b31ea4c
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-21 04:01:03 -0400

    output v1

--- Sites/consultas/consulta_1.php
+++ Sites/consultas/consulta_1.php
@@ -18,13 +18,14 @@ ini_set('display_errors', 1);
     #AND direccion_clientes.id_cliente = clientes.id
     #AND DATE(fecha_entrega) = $fecha_seleccionada;"
 
-    $query =  "SELECT clientes.nombre, clientes.calle, clientes.region, clientes.comuna FROM despachos, compras, clientes 
+    $query =  "SELECT clientes.nombre, clientes.calle, clientes.numero_domimcilio clientes.region, clientes.comuna 
+    FROM despachos, compras, clientes 
     WHERE despachos.id_compra = compras.id_compra and compras.id_cliente = clientes.id_cliente 
     and despachos.fecha_entrega = '$fecha_seleccionada';";
     print_r($_POST);
 
-	$result = $db -> prepare($query);
-	$result -> execute();
+	#$result = $db -> prepare($query);
+	#$result -> execute();
     var_dump($result->errorInfo());
 	$clientes = $result -> fetchAll();
     print_r($clientes);
@@ -36,13 +37,14 @@ ini_set('display_errors', 1);
     <tr>
       <th>Nombre</th>
       <th>Calle</th>
-      <th>Calle</th>
-      <th>Calle</th>
+      <th>Numero domiciliio</th>
+      <th>Region</th>
+      <th>Comuna</th>
 
     </tr>
   <?php
 	foreach ($clientes as $cliente) {
-  	echo "<tr><td>$cliente[0]</td><td>$cliente[1]</td> <td>$cliente[2]</td><td>$cliente[3]</td></tr>";
+  	echo "<tr><td>$cliente[0]</td><td>$cliente[1]</td> <td>$cliente[2]</td><td>$cliente[3]</td><td>$cliente[4]</td></tr>";
 	}
   ?>
 	</table>
