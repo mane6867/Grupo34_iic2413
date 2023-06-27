498fb8be6746e5463cf82959d3c09e5c0f7fae8e
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-26 21:41:46 -0400

    debuggeo

---
+++ Sites/importar_usuarios.php
@@ -0,0 +1,9 @@
+<?php
+
+require("../config/conexion.php");
+include('../templates/header.html');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
