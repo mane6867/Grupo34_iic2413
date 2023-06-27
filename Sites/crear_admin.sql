a62b1c74ffd2c4a60b220044ad98080449b52607
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-26 23:20:53 -0400

    retorno

--- Sites/crear_admin.sql
+++ Sites/crear_admin.sql
@@ -1,11 +1,14 @@
 CREATE OR REPLACE FUNCTION crear_admin()
 
-RETURNS void AS $$
+RETURNS BOOLEAN AS $$
 
 BEGIN
     -- verificar si existe o no el admin
     IF NOT EXISTS (SELECT 1 FROM usuarios WHERE tipo = 'A' AND nombre_usuario = 'admin') THEN
         INSERT INTO usuarios VALUES ( 0, 'ADMIN', 'A', 'admin');
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
     END IF;
 END
 
