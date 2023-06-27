90451802d3995b92cad17fa0a595c2302d4d9659
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-26 20:07:41 -0400

    Boton importar usuarios

--- Sites/config/data.php
+++ Sites/config/data.php
@@ -2,6 +2,7 @@
   $user34 = 'grupo34';
   $password34 = 'maneisa7';
   $databaseName34 = 'grupo34e3';
+  
   $user65 = 'grupo65';
   $password65 = '123456789';
   $databaseName65 = 'grupo65e3';
--- Sites/crear_admin.sql
+++ Sites/crear_admin.sql
@@ -7,8 +7,9 @@ RETURNS void AS $$
 BEGIN
     -- verificar si existe o no el admin 
     IF 'ADMIN;admin;Admin' not in usuarios THEN
-    insert into usuarios values('ADMIN','admin','Admin');
+        insert into usuarios values('ADMIN','admin','Admin');
     END IF;
+    -- clientes a usuarios
 END
 
 $$ language plpqsql
