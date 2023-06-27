0b13aea26b7e90b8dff422a1bb0cda0dd899d100
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-27 00:03:10 -0400

    inicio login

--- Sites/index.php
+++ Sites/index.php
@@ -5,11 +5,28 @@
   <h1 align="center">Muebles3465</h1>
 
 
+  
+
+
+
     <div class='container'>
         <form  action='./importar_usuarios.php' method='GET'>
             <input class='btn' type='submit' value='Importar Usuarios'>
         </form>
     </div>
+
+  
+    <h3 align="center"> Login Usuario</h3>
+
+  <form align="center" action="portal_usuarios.php" method="get">
+      Nombre:
+      <input type="text" name="nombre">
+      <br/>
+      Contraseña:
+      <input type="text" name="contrasena">
+      <br/>
+      <input type="submit" value="Buscar">
+</form>
 
   <br>
   <br>
