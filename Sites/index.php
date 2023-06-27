b693cdd9cbcfb426653a2c0259fbb20ef9d051d9
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-27 00:33:55 -0400

    hola

--- Sites/index.php
+++ Sites/index.php
@@ -18,7 +18,9 @@
   
     <h3 align="center"> Login Usuario</h3>
 
-  <form align="center"  method="post">
+
+    <div class='box'>
+    <form align="center"  method="post">
       Nombre:
       <input type="text" name="nombre">
       <br/>
@@ -26,7 +28,8 @@
       <input type="text" name="contrasena">
       <br/>
       <input type="submit" value="Ingresar">
-  </form>
+    </form>
+    </div>
 
 <?php session_start(); ?>
 <?php
