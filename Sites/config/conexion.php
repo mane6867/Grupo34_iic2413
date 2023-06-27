245d3cf6bf5c0819785f8dac5b19c77235b44996
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-26 20:33:18 -0400

    Arreglo require

--- Sites/config/conexion.php
+++ Sites/config/conexion.php
@@ -3,7 +3,7 @@
   # BBDD Grupo 34
   try {
     #Pide las variables para conectarse a la base de datos.
-    require('data34.php'); 
+    require('data.php'); 
     # Se crea la instancia de PDO
     $db34 = new PDO("pgsql:dbname=$databaseName34;host=localhost;port=5432;user=$user34;password=$password34");
   } catch (Exception $e) {
@@ -13,7 +13,7 @@
   # BBDD Grupo 65
   try {
     #Pide las variables para conectarse a la base de datos.
-    require('data65.php'); 
+    require('data.php'); 
     # Se crea la instancia de PDO
     $db65 = new PDO("pgsql:dbname=$databaseName65;host=localhost;port=5432;user=$user65;password=$password65");
   } catch (Exception $e) {
