b4ce3e8ea55a77042b59ccece827c4ce1f49e00a
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-26 21:44:17 -0400

    requirec1

--- Sites/importar_usuarios.php
+++ Sites/importar_usuarios.php
@@ -1,5 +1,8 @@
+<body>
 <?php
-
-require("../config/conexion.php");
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+    require("../config/conexion.php");
 ?>
+</body>
