25d4065b7064c38e787ae81707a3a10871c79bfb
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-26 23:24:06 -0400

    nombre

--- Sites/crear_admin.sql
+++ Sites/crear_admin.sql
@@ -4,7 +4,7 @@ RETURNS BOOLEAN AS $$
 
 BEGIN
     -- verificar si existe o no el admin
-    IF NOT EXISTS (SELECT 1 FROM usuarios WHERE tipo = 'A' AND nombre_usuario = 'ADMIN') THEN
+    IF NOT EXISTS (SELECT 1 FROM usuarios WHERE tipo = 'A' AND nombre = 'ADMIN') THEN
         INSERT INTO usuarios VALUES ( 0, 'ADMIN', 'A', 'admin');
         RETURN TRUE;
     ELSE
