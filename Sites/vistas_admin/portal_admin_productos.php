0fa371258a9529d8a834759fcdce0b64d96c2031
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-27 23:13:18 -0400

    print r

--- Sites/vistas_admin/portal_admin_productos.php
+++ Sites/vistas_admin/portal_admin_productos.php
@@ -10,13 +10,14 @@ $categoria = $_POST['categoria'];
 $_SESSION['categoria_producto'] = $categoria;
 $id_tienda = $_SESSION['id_tienda'];
 print_r($_SESSION['categoria_producto']);
+print_r($_SESSION['id_tienda']);
 
 
-$query = "SELECT * FROM productos, stock WHERE productos.categoria = '$categoria' and stock.id_tienda = $id_tienda;";
-$result = $db65 -> prepare($query);
-$result -> execute();
-$productos = $result -> fetchAll();
-print_r($productos);
+//$query = "SELECT * FROM productos, stock WHERE productos.categoria = '$categoria' and stock.id_tienda = $id_tienda;";
+//$result = $db65 -> prepare($query);
+//$result -> execute();
+//$productos = $result -> fetchAll();
+//print_r($productos);
 
 // Obtener las regiones
 
