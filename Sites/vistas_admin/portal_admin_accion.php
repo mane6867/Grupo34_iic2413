7b4bfef88ffb7bf9cbd387de69621c5be6157612
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-27 23:48:08 -0400

    acciones admins

--- Sites/vistas_admin/portal_admin_accion.php
+++ Sites/vistas_admin/portal_admin_accion.php
@@ -14,8 +14,17 @@ print_r($_SESSION['categoria_producto']);
 print_r($_SESSION['id_tienda']);
 
 ?>
-
-<html>
+<html lang="en">
+<body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+    crossorigin="anonymous">
+    <link href="styles/mystyles.css" rel="stylesheet" type="text/css">
+    <title>Muebles3465</title>
+</head>
 
 <div class="container">
     <div class="text-center" style="background-color: #aaa;">
@@ -29,14 +38,22 @@ print_r($_SESSION['id_tienda']);
                     <input type="text" name="stock"placeholder="Nuevo stock" required>
                 </div>
                 <div class="login-elements">
-                    <input type="password" name="contrasena"placeholder="Contraseña" required>
+                    <input type="submit" name="Actualizar" class="btn btn-success">
+                </div>
+
+                <div class="formulario-login-cabecera">
+                    <p class="formulario-login-fuente-cabecera">Armar oferta</p>
                 </div>
                 <div class="login-elements">
-                    <input type="submit" name="Ingresar" class="btn btn-success">
+                    <input type="text" name="stock"placeholder="Precio oferta" required>
+                </div>
+                <div class="login-elements">
+                    <input type="submit" name="Crear" class="btn btn-success">
                 </div>
             </form>
         </div>
     </div>
 </div>
+</body>
 
 </html>
