2e83a26e3bffc82a3d879ba9fed2ff07758fd730
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-28 00:04:13 -0400

    arreglo paths

--- Sites/vistas_admin/portal_admin_stock.php
+++ Sites/vistas_admin/portal_admin_stock.php
@@ -1,11 +1,11 @@
 <html>
-<?php include('templates/header.html');   ?>
+<?php include('../templates/header.html');   ?>
 <body>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
     #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require("config/conexion.php");
+    require("../config/conexion.php");
 
     // Primero obtenemos los datos del producto a actualizar
     session_start();
