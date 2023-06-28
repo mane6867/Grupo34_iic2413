941d02a0e1c66d47b6e07f84b321216d03f9855b
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-27 21:13:35 -0400

    dropdown regiones

--- Sites/portal_admin.php
+++ Sites/portal_admin.php
@@ -1,11 +1,15 @@
-Hola Admin
+<?php
+// Obtener las regiones
+$query = "SELECT id_region, nombre FROM region ORDER BY ASC;";
+$result = $db34 -> prepare($query);
+$result -> execute();
+$regiones = $result -> fetchAll();
+?>
 
 <html>
-<label for="color">Background Color:</label>
-<select name="color" id="color">
-	<option value="">--- Choose a color ---</option>
-	<option value="red">Red</option>
-	<option value="green">Green</option>
-	<option value="blue">Blue</option>
+<select name="region">
+    <?php foreach ($regiones as $region): ?>
+        <option value="<?php echo $region[0]; ?>"><?php echo $region[1]; ?></option>
+    <?php endforeach; ?>
 </select>
 </html>
