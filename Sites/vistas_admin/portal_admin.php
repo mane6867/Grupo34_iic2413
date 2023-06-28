2165fb868ae0b5b1fc98bf5c7b52a1643b0aa1f0
Isabel Jofré Birke <isabeljofre@200-138-221-eduroam-al.wlan.net.uc.cl>
2023-06-28 16:46:08 -0400

    sd

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -1,12 +1,14 @@
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
-    <title>Muebles3465</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+  crossorigin="anonymous">
+  <link href="styles/mystyles.css" rel="stylesheet" type="text/css">
+  <title>Muebles3465</title>
 </head>
+
 
 <?php
 error_reporting(E_ALL);
