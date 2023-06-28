bde19d802a4a09e1877ca94fe666ac71a00a1bd8
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-27 20:59:27 -0400

    admin

--- Sites/portal_admin.php
+++ Sites/portal_admin.php
@@ -1,1 +1,11 @@
 Hola Admin
+
+<html>
+<label for="color">Background Color:</label>
+<select name="color" id="color">
+	<option value="">--- Choose a color ---</option>
+	<option value="red">Red</option>
+	<option value="green">Green</option>
+	<option value="blue">Blue</option>
+</select>
+</html>
