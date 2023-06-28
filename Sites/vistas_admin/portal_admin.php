0a3245d49c643e213e328098618509915443fa6f
Isabel Jofré Birke <isabeljofre@200-138-221-eduroam-al.wlan.net.uc.cl>
2023-06-28 16:35:06 -0400

    fg

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -10,8 +10,6 @@ $result -> execute();
 $regiones = $result -> fetchAll();
 ?>
 
-<?php include('../templates/header.html'); ?>
-
 <html>
 <head>
     <meta charset="UTF-8">
@@ -19,7 +17,7 @@ $regiones = $result -> fetchAll();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
     crossorigin="anonymous">
-    <link href="./styles/mystyles.css" rel="stylesheet" type="text/css">
+    <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
     <title>Muebles3465</title>
 </head>
 
