210987bde2c6a64ec1ae613912b79138ff89bc01
Isabel Jofré Birke <isabeljofre@200-138-221-eduroam-al.wlan.net.uc.cl>
2023-06-28 15:36:11 -0400

    dropdowns

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -11,6 +11,17 @@ $regiones = $result -> fetchAll();
 ?>
 
 <html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+    crossorigin="anonymous">
+    <link href="styles/mystyles.css" rel="stylesheet" type="text/css">
+    <title>Muebles3465</title>
+</head>
+
 <form align="center" method="POST" action="portal_admin_tiendas.php">
     Seleccione una región:
     <select name="region">
