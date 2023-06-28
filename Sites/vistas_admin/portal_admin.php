39ed87799a18a1304277d76e32e4797e1d86fdac
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-27 21:53:26 -0400

    s

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -11,7 +11,7 @@ $regiones = $result -> fetchAll();
 ?>
 
 <html>
-<form align="center" method="POST" action="portal_admin_tienda.php">
+<form align="center" method="POST" action="portal_admin_tiendas.php">
     Seleccione una región:
     <select name="region">
     <?php foreach ($regiones as $region): ?>
