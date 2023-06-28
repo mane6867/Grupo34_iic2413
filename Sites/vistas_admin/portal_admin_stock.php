ae2764baa8935e3c69722e3bbfbc27c40984315b
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-28 00:13:22 -0400

    stock:

--- Sites/vistas_admin/portal_admin_stock.php
+++ Sites/vistas_admin/portal_admin_stock.php
@@ -1,14 +1,22 @@
-<html>
-<?php include('../templates/header.html');   ?>
-<body>
-<?php
+<?php include('../templates/header.html');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+
+    # Llama a conexión, crea el objeto PDO y obtiene la variable $db
     require("../config/conexion.php");
 
     // Primero obtenemos los datos del producto a actualizar
     session_start();
-    print($_SESSION['id_producto'])
+    $id_producto = $_SESSION['id_producto'];
+    $nuevo_stock = $_POST['stock'];
+    $id_tienda = $_SESSION['id_tienda'];
+
+
+    $query = "SELECT * FROM stock WHERE stock.id_producto = $id_producto AND stock.id_tienda = $id_tienda;";
+    $result = $db65 -> prepare($query);
+    $result -> execute();
+    $datos = $result -> fetchAll();
+
+    print($datos)
 
 ?>
