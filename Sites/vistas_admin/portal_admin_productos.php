384d49500835671a4a606590278f3a1464c86448
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-27 22:30:57 -0400

    intento 2 productos

--- Sites/vistas_admin/portal_admin_productos.php
+++ Sites/vistas_admin/portal_admin_productos.php
@@ -6,9 +6,18 @@ ini_set('display_errors', 1);
 // Obtener las regiones
 
 
-$query = "SELECT * FROM productos WHERE categoria = $_SESSION['id_tienda'] ;";
-$result = $db65 -> prepare($query);
-$result -> execute();
-$productos = $result -> fetchAll();
-print_r($productos)
+//$query = "SELECT * FROM productos WHERE categoria = $_SESSION['id_tienda'] ;";
+//$result = $db65 -> prepare($query);
+//$result -> execute();
+//$productos = $result -> fetchAll();
+//print_r($productos)
+
+// Obtener las regiones
+$query = "SELECT * FROM productos WHERE categoria = :id_tienda;";
+$result = $db65->prepare($query);
+$result->bindParam(':id_tienda', $_SESSION['id_tienda'], PDO::PARAM_INT);
+$result->execute();
+$productos = $result->fetchAll();
+print_r($productos);
+?>
 ?>
