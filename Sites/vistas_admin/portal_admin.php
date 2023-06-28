6d33e4fec23aa98c16421aad6a00be9c92ff4585
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-28 16:12:54 -0400

    hkhj

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -48,4 +48,17 @@ $regiones = $result -> fetchAll();
     <input type="submit" value="Enviar">
     </select>
 </form>
+
+<style>
+
+select {
+    padding: 10px;
+    font-size: 16px;
+    border: 10px solid #961e1e;
+    border-radius: 4px;
+}
+option {
+    padding: 5px;
+}
+</style>
 </html>
