b66145ceb1b2777d56334c318c9f1c1cfc3d678a
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-27 22:40:55 -0400

    consulta producto

--- Sites/vistas_admin/portal_admin_productos.php
+++ Sites/vistas_admin/portal_admin_productos.php
@@ -3,21 +3,20 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
     #Llama a conexión, crea el objeto PDO y obtiene la variable $db
     require("../config/conexion.php");
+
+
+//guardar categoria
+$categoria = $_POST['categoria']
+$_SESSION['categoria_producto'] = $categoria;
+
+
+$query = "SELECT * FROM productos, stock WHERE stock.id_tienda = $_SESSION['id_tienda'] AND productos.categoria = $_SESSION['categoria'];";
+$result = $db65 -> prepare($query);
+$result -> execute();
+$productos = $result -> fetchAll();
+print_r($productos)
+
 // Obtener las regiones
 
-
-//$query = "SELECT * FROM productos WHERE categoria = $_SESSION['id_tienda'] ;";
-//$result = $db65 -> prepare($query);
-//$result -> execute();
-//$productos = $result -> fetchAll();
-//print_r($productos)
-
-// Obtener las regiones
-$query = "SELECT * FROM productos WHERE categoria = :id_tienda;";
-$result = $db65->prepare($query);
-$result->bindParam(':id_tienda', $_SESSION['id_tienda'], PDO::PARAM_INT);
-$result->execute();
-$productos = $result->fetchAll();
-print_r($productos);
 ?>
 ?>
