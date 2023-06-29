3ce87223c48c960517ee73b01bd809277494b65a
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-29 00:57:44 -0400

    tiendas

--- Sites/vistas_admin/portal_admin_tiendas.php
+++ Sites/vistas_admin/portal_admin_tiendas.php
@@ -14,20 +14,61 @@ $result -> execute();
 $tiendas = $result -> fetchAll();
 ?>
 
+
 <html>
+<head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50vh;
+    }
+    .form-container {
+      background-color: #eee;
+      padding: 20px;
+      width: 400px;
+      border: 2px solid #ccc;
+      height: 280px; /* Ajusta la altura según tus necesidades */
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-top: 120px;
+    }
+    .portal-title {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 30px;
+    }
+  </style>
+</head>
+
+
+
+<div class = "container">
+    <div class = "form-container">
+    <div class="portal-title">Portal Administrador</div>
 <form align="center" method="POST" action="portal_admin_categorias.php">
+    <div>
     Seleccione una tienda:
+    </div>
+    <div class="select is-success">
     <select name="tienda">
-    <option value="">-> Seleccione una tienda</option>
     <?php foreach ($tiendas as $tienda): ?>
         <option value="<?php echo $tienda[0]; ?>"><?php echo $tienda[0]; ?></option>
     <?php endforeach; ?>
     </select>
     <br>
-    <input type="submit" value="Enviar">
-</select>
+    <input class="button is-success" type="submit" value="Enviar">
+    </br>
+    </select>
+    </div>
 </form>
-
+</div>
+</div>
 </html>
 
 
