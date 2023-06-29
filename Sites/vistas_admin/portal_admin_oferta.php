96f84be4388fea2d295b6d5241f3b10c3facbdb6
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-28 23:37:44 -0400

    estilo isa intento 1

--- Sites/vistas_admin/portal_admin_oferta.php
+++ Sites/vistas_admin/portal_admin_oferta.php
@@ -55,27 +55,48 @@ ini_set('display_errors', 1);
 
 
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+    crossorigin="anonymous">
+    <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
+    <title>Muebles3465</title>
+</head>
 
 
-<div class="columns">
-  <div class="column">
-  </div>
+<div class="container">
+    <div class="text-center" style="background-color: #aaa;">
+        <p class="formulario-login-fuente-cabecera">Oferta</p>
+        <div class="formulario-login">
+            <form action="portal_admin.php" method="GET">
+                <div class="formulario-login-cabecera">
+                    <p class="formulario-login-fuente-cabecera">La oferta ha sido realizada</p>
+                </div>
 
-  <div class="column">
-    <div class="box">
+                <div class="login-elements">
+                <p> <strong>Precio sin descuento: </strong><?php echo $precio_sin_descuento; ?></p>
+                <p> <strong> Descuento: </strong> <?php echo $descuento_nuevo; ?></p>
+                <p><strong> Precio con descuento: </strong> <?php echo $precio_descuento_nuevo; ?></p>
+                </div>
+                <div class="login-elements">
+                    <input type="submit" name="Volver" class="btn btn-success">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
         <h2> La oferta ha sido recibida</h2>
         <p> Precio sin descuento: <?php echo $precio_sin_descuento; ?></p>
         <p> Descuento: <?php echo $descuento_nuevo; ?></p>
         <p> Precio con descuento: <?php echo $precio_descuento_nuevo; ?></p>
-    </div>
-  </div>
 
-  <div class="column">
-  </div>
 
-</div>
 
 <table>
     <tr>
