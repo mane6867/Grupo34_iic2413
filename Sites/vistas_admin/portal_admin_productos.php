e39f08211158b212d32d207dd4dc9f07bbf6bba4
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-29 01:06:17 -0400

    productos

--- Sites/vistas_admin/portal_admin_productos.php
+++ Sites/vistas_admin/portal_admin_productos.php
@@ -10,8 +10,6 @@ session_start();
 $categoria = $_POST['categoria'];
 $_SESSION['categoria_producto'] = $categoria;
 $id_tienda = $_SESSION['id_tienda'];
-print_r($_SESSION['categoria_producto']);
-print_r($_SESSION['id_tienda']);
 
 
 $query = "SELECT * FROM productos, stock WHERE productos.categoria = '$categoria' AND stock.id_tienda = $id_tienda 
@@ -24,17 +22,56 @@ $productos = $result -> fetchAll();
 
 <html>
 
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50vh;
+    }
+    .form-container {
+      background-color: #eee;
+      padding: 20px;
+      width: 400px;
+      border: 2px solid #ccc;
+      height: 280px; /* Ajusta la altura según tus necesidades */
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-top: 120px;
+    }
+    .portal-title {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 30px;
+    }
+  </style>
+</head>
+
+
+<div class = "container">
+    <div class = "form-container">
+    <div class="portal-title">Portal Administrador</div>
 <form align="center" method="POST" action="portal_admin_accion.php">
+    <div>
     Seleccione un producto:
+    </div>
+    <div class="select is-success">
     <select name="producto">
-    <option value="">Producto</option>
     <?php foreach ($productos as $producto): ?>
         <option value="<?php echo $producto[0]; ?>"><?php echo $producto[1]; ?></option>
     <?php endforeach; ?>
     </select>
     <br>
-    <input type="submit" value="Enviar">
-</select>
+    <input class="button is-success" type="submit" value="Enviar">
+    </br>
+    </select>
+    </div>
 </form>
-
+</div>
+</div>
 </html>
