eaa539a644bc5b14805910de3287d86774db2531
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-29 00:18:57 -0400

    otra vez

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -1,15 +1,9 @@
 <html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-  crossorigin="anonymous">
-  <link href="styles/mystyles.css" rel="stylesheet" type="text/css">
-  <title>Muebles3465</title>
 
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 
@@ -28,15 +22,13 @@ $regiones = $result -> fetchAll();
 
 
 
-<!--<div class = 'container'>
 <form align="center" method="POST" action="portal_admin_tiendas.php">
     Seleccione una región:
     <div class="select is-info">
     <select name="region">
-    <option value="">Región</option>
-    <?php //foreach ($regiones as $region): ?>
-        <option value="<?php //echo $region[1]; ?>"><?php //echo $region[1]; ?></option>
-    <?php //endforeach; ?>
+    <?php foreach ($regiones as $region): ?>
+        <option value="<?php echo $region[1]; ?>"><?php echo $region[1]; ?></option>
+    <?php endforeach; ?>
     </select>
     <br>
     <input type="submit" value="Enviar">
@@ -44,9 +36,8 @@ $regiones = $result -> fetchAll();
     </select>
     </div>
 </form>
-</div> -->
 
-
+<!--
 <div class="container">
     <div class="text-center" style="background-color: #aaa;">
         <p class="formulario-login-fuente-cabecera">Portal Administrador</p>
@@ -70,6 +61,7 @@ $regiones = $result -> fetchAll();
         </div>
     </div>
 </div>
+-->
 
 
 </html>
