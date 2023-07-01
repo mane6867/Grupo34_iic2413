9d59a32ba74a6e8771d05f36da6c22943195d209
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-30 23:49:56 -0400

    estilos

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -3,6 +3,7 @@
 
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
     .container {
