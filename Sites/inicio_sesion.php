b914f05d6fc7f69ed702be6da5f571aa14f6b0d4
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-30 23:08:29 -0400

    login

--- Sites/inicio_sesion.php
+++ Sites/inicio_sesion.php
@@ -12,6 +12,6 @@ $query = "SELECT * FROM usuarios
 $consulta = $db34 -> prepare($query);
 $consulta -> execute();
 $cantidad = $consulta -> fetchAll();
-print_r($cantidad);
+print_r($cantidad[1]);
 
 ?>
