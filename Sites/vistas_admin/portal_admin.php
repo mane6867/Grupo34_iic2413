2689d343ecd51acee76a007266c118edc02e1de3
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-07-01 00:01:17 -0400

    estilos

--- Sites/vistas_admin/portal_admin.php
+++ Sites/vistas_admin/portal_admin.php
@@ -3,32 +3,8 @@
 
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-    .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 50vh;
-    }
-    .form-container {
-      background-color: #eee;
-      padding: 20px;
-      width: 400px;
-      border: 2px solid #ccc;
-      height: 370px; /* Ajusta la altura según tus necesidades */
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      margin-top: 190px;
-    }
-    .portal-title {
-      font-size: 24px;
-      font-weight: bold;
-      margin-bottom: 30px;
-    }
-  </style>
 
 </head>
 
