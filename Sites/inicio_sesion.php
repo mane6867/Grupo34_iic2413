fea13604e1412d38d188fc1f5c3ee2273b77cacf
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-30 22:29:16 -0400

    login

--- Sites/inicio_sesion.php
+++ Sites/inicio_sesion.php
@@ -1,21 +1,7 @@
 <?php
 
 session_start();
-
-$nombre_login = $_POST['nombre'];
-$clave_login = $_POST['contrasena'];
-
-$query = ("SELECT * FROM usuarios WHERE nombre = '$nombre_login' AND clave = '$clave_login';");
-$consulta = $db34 -> prepare($query);
-$consulta -> execute();
-$cantidad = pg_num_rows($consulta);
-
-if($cantidad > 0){
-    $_SESSION['nombre_usuario'] = $nombre_login;
-    header('Location:portal_usuarios.php');
-}
-else{
-    echo "Nombre de usuario o clave inválida";
-}
+$usuario = $_SESSION['nombre_usuario'];
+echo "<h2>Bienvenido $usuario </h2>";
 
 ?>
