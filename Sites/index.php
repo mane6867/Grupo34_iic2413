b3770ee66cf9f45323f867ba87f24d4f95ae8eb1
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-07-01 00:08:00 -0400

    estilos

--- Sites/index.php
+++ Sites/index.php
@@ -3,34 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+  crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link href="styles/mystyles.css" rel="stylesheet" type="text/css">
   <title>Muebles3465</title>
 </head>
-<style>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 50vh;
-    }
-    .form-container {
-        background-color: #eee;
-        padding: 20px;
-        width: 400px;
-        border: 2px solid #ccc;
-        height: 370px; /* Ajusta la altura según tus necesidades */
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin-top: 190px;
-    }
-    .portal-title {
-        font-size: 24px;
-        font-weight: bold;
-        margin-bottom: 30px;
-    }
-</style>
+
 
 <div class="container">
     <div class="text-center" style="background-color: #eee;">
