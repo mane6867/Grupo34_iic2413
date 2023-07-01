51275a273e7c3027315eafdb285b2046b48f78b7
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-07-01 19:02:31 -0400

    crear sesion

--- Sites/crear_sesion.php
+++ Sites/crear_sesion.php
@@ -16,6 +16,16 @@ $query = "SELECT crear_cuenta($nombre, $rut, $region, $comuna, $calle, $numero_c
 $consulta = $db34 -> prepare($query);
 $consulta -> execute();
 
+$query = "SELECT id_cliente FROM clientes WHERE nombre = $nombre AND rut = $rut;";
+$consulta = $db34 -> prepare($query);
+$consulta -> execute();
+$resultado = $consulta -> fetchAll();
+$id_cliente = $resultado[0][0];
+
+$query = "SELECT importar_cliente($id_cliente, '$nombre', 'C');";
+$result = $db34 -> prepare($query);
+$result -> execute();
+
 header('Location:portal_usuarios.php');
 
 ?>
