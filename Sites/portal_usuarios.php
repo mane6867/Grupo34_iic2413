60e1b97e9daa0d3b3c56e958930bf311a1f7fe3c
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-30 23:16:15 -0400

    login

--- Sites/portal_usuarios.php
+++ Sites/portal_usuarios.php
@@ -2,7 +2,7 @@
 
 session_start();
 $usuario = $_SESSION['nombre_usuario'];
-echo "<h2>Bienvenido $usuario </h2>";
+echo "<h2>Bienvenido $usuario $clave</h2>";
 echo "estas en portal usuario";
 
 ?>
