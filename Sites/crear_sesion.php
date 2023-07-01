7b04bc89f072b2b244456d6b52a0a155429ff8e7
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-07-01 18:27:03 -0400

    archivo crear_sesion.php

--- Sites/crear_sesion.php
+++ Sites/crear_sesion.php
@@ -1,1 +1,21 @@
-<?echo 'hola';?>
+<?php
+
+require("config/conexion.php");
+
+session_start();
+$comuna = $_POST['comuna'];
+$calle = $_POST['calle'];
+$numero_calle = $_POST['numero'];
+$nombre = $_SESSION['nombre'];
+$rut = $_SESSION['rut'];
+$region = $_SESSION['region'];
+
+
+
+$query = "SELECT crear_cuenta($nombre, $rut, $region, $comuna, $calle, $numero_calle);";
+$consulta = $db34 -> prepare($query);
+$consulta -> execute();
+
+header('Location:portal_usuarios.php');
+
+?>
