c1ad0839031dabac9207a54ce183a66979cfb9b4
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-07-01 15:23:41 -0400

    head style import 2

--- Sites/importar_usuarios.php
+++ Sites/importar_usuarios.php
@@ -1,10 +1,4 @@
 <html>
-  
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-  crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 <?php include('templates/header.html');   ?>
 
@@ -73,7 +67,9 @@ ini_set('display_errors', 1);
 </div>
 </div>
 
-
+<br>
+<br>
+<br>
 </body>
 <form  align="center" action="index.php" method="get">
     <input type="submit" value="Volver" class="button is-success">
