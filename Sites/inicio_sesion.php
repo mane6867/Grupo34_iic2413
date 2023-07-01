7da2f013e74112841f42a971e48bd22995895562
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-30 23:15:31 -0400

    login

--- Sites/inicio_sesion.php
+++ Sites/inicio_sesion.php
@@ -13,7 +13,10 @@ $consulta = $db34 -> prepare($query);
 $consulta -> execute();
 $cantidad = $consulta -> fetchAll();
 
-if($cantidad[0] > 0){
+if($usuario == "ADMIN" && $clave == "admin"){
+    header('Location:vistas_admin/portal_admin.php');
+}
+elseif($cantidad[0] > 0){
     $_SESSION['nombre_usuario'] = $usuario;
     header('Location:portal_usuarios.php');
 }
