48cd0cf9362ff29de92a190ae9d7913ad3854927
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-06-30 21:48:00 -0400

    page stock

--- Sites/vistas_admin/portal_admin_stock.php
+++ Sites/vistas_admin/portal_admin_stock.php
@@ -16,6 +16,8 @@ ini_set('display_errors', 1);
     $result -> execute();
     $datos = $result -> fetchAll();
 
+    $antiguo_stock = $datos[0][3];
+
     $query = "SELECT actualizar_stock($id_tienda, $id_producto, $nuevo_stock);";
     $result = $db65 -> prepare($query);
     $result -> execute();
@@ -27,22 +29,37 @@ ini_set('display_errors', 1);
 
 ?>
 
-<html>
+<html lang="en">
+<body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+    crossorigin="anonymous">
+    <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
+    <title>Muebles3465</title>
+</head>
 
-<table>
-    <tr>
-        <th>Producto</th>
-        <th>Tienda</th>
-        <th>Cantidad</th>
-    </tr>
-    <?php
-        foreach ($datos as $dato) {
-            echo "<tr><td>$dato[1]</td><td>$dato[2]</td> <td>$dato[3]</td></tr>";
-        }
-        foreach ($datos2 as $dato) {
-            echo "<tr><td>$dato[1]</td><td>$dato[2]</td> <td>$dato[3]</td></tr>";
-        }
-    ?>
-</table>
+
+<div class="container">
+    <div class="text-center" style="background-color: #eee;">
+        <p class="formulario-login-fuente-cabecera">El stock ha sido actualizado</p>
+        <div class="formulario-login">
+            <form action="portal_admin.php" method="GET">
+
+                <div class="login-elements">
+                <p> <strong>Stock antiguo: </strong><?php echo $antiguo_stock; ?></p>
+                <p> <strong>Stock actual: </strong> <?php echo $nuevo_stock; ?></p>
+                </div>
+                <div class="login-elements">
+                    <input type="submit" value="Volver" class="btn btn-success">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 
 </html>
+
