16960ecf2a4ab3bb43f73f479d4aec19fb20eb49
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-06-30 23:02:59 -0400

    login

--- Sites/inicio_sesion.php
+++ Sites/inicio_sesion.php
@@ -6,8 +6,6 @@ session_start();
 $usuario = $_POST['nombre'];
 $clave = $_POST['contrasena'];
 
-echo "Bienvendio $usuario $clave";
-
 $query = "SELECT * FROM usuarios 
     WHERE usuarios.nombre = '$usuario'
     AND usuarios.clave = '$clave';";
@@ -15,11 +13,12 @@ $consulta = $db34 -> prepare($query);
 $consulta -> execute();
 $cantidad = $consulta -> fetchAll();
 
-echo "$cantidad";
-
 if($cantidad > 0){
     $_SESSION['nombre_usuario'] = $usuario;
     header('Location:portal_usuarios.php');
+}
+else{
+    echo "Datos incorrectos :("
 }
 
 ?>
