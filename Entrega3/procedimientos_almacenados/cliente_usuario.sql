9023f94ae5d2779014ad229c01946eb3ad917441
MAGDALENA PINO <magdalena.pino@uc.cl>
2023-07-02 16:26:40 -0400

    contraseña

--- Entrega3/procedimientos_almacenados/cliente_usuario.sql
+++ Entrega3/procedimientos_almacenados/cliente_usuario.sql
@@ -11,7 +11,7 @@ i INT;
 
 BEGIN
     FOR i IN 1..10 LOOP 
-        contrasena := contrasena || substr(characters, ceil(random() * length(characters)), 1);
+        contrasena := contrasena || substring(characters, floor(random() * length(characters) + 1)::int, 1);
     END LOOP;
 
     IF id NOT IN (SELECT id_usuario from usuarios) THEN
