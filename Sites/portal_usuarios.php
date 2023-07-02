52915e97e9d33ca4fb018819a742c5cc51398378
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-07-02 17:58:00 -0400

    estilos

--- Sites/portal_usuarios.php
+++ Sites/portal_usuarios.php
@@ -1,35 +1,33 @@
 <html>
 <head>
-
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 50vh;
-    }
-    .form-container {
-        background-color: #eee;
-        padding: 20px;
-        width: 400px;
-        border: 2px solid #ccc;
-        height: 370px; /* Ajusta la altura según tus necesidades */
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin-top: 190px;
-    }
-    .portal-title {
-        font-size: 24px;
-        font-weight: bold;
-        margin-bottom: 30px;
-    }
-</style>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="styles/mystyles.css" rel="stylesheet" type="text/css">
+    <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 50vh;
+        }
+        .form-container {
+            background-color: #eee;
+            padding: 20px;
+            width: 400px;
+            border: 2px solid #ccc;
+            height: 370px; /* Ajusta la altura según tus necesidades */
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-top: 190px;
+        }
+        .portal-title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 30px;
+        }
+    </style>
 </head>
 
 
