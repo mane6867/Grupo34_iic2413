4682f471acbb40750cea47ae459e8de28a3b4598
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-07-02 12:30:43 -0400

    estilos

--- Sites/vistas_cliente/crear_cliente_inicio.php
+++ Sites/vistas_cliente/crear_cliente_inicio.php
@@ -1,34 +1,35 @@
 <html>
-<head>
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Muebles3465</title>
+    <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 50vh;
+        }
+        .form-container {
+            background-color: #eee;
+            padding: 20px;
+            width: 400px;
+            border: 2px solid #ccc;
+            height: 370px; /* Ajusta la altura según tus necesidades */
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-top: 190px;
+        }
+        .portal-title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 30px;
+        }
+    </style>
 </head>
-<style>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 50vh;
-    }
-    .form-container {
-        background-color: #eee;
-        padding: 20px;
-        width: 400px;
-        border: 2px solid #ccc;
-        height: 370px; /* Ajusta la altura según tus necesidades */
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin-top: 190px;
-    }
-    .portal-title {
-        font-size: 24px;
-        font-weight: bold;
-        margin-bottom: 30px;
-    }
-</style>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -67,9 +68,6 @@ $regiones = $result -> fetchAll();
 
             </div>
         </form>
-        <br>
-        <br>
-        <br>
         <form  align="center" action="../index.php" method="get">
             <input type="submit" value="Volver" class="button is-success">
         </form>
