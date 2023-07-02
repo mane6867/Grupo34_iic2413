ca69bf4ee7b147df04a7c180d4eb0b44cbccdad7
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-07-02 12:20:31 -0400

    estilos

--- Sites/vistas_cliente/crear_cliente_comuna.php
+++ Sites/vistas_cliente/crear_cliente_comuna.php
@@ -1,8 +1,13 @@
 <html>
 <head>
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link href="../styles/mystyles.css" rel="stylesheet" type="text/css">
+    <title>Muebles3465</title>
 </head>
 <style>
     .container {
